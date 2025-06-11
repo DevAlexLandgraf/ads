4e8d9e0fc3f649047be1af4149dd58b41414f721
Alex Landgraf <150557773+DevAlexLandgraf@users.noreply.github.com>
2025-06-11 15:06:19 -0300

    Update Integration.java

--- ironsource-ads-sdk/src/main/java/com/solodroidx/ads/util/Integration.java
+++ ironsource-ads-sdk/src/main/java/com/solodroidx/ads/util/Integration.java
@@ -1,3 +1,7 @@
 package com.solodroidx.ads.util;
 
 import com.ironsource.mediationsdk.integration.IntegrationHelper;
+
+public class Integration {
+  IntegrationHelper.validateIntegration(this);
+}
