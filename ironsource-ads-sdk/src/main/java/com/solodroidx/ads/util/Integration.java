3241352306ebef865f44778485809f74d9f76503
Alex Landgraf <150557773+DevAlexLandgraf@users.noreply.github.com>
2025-06-11 15:09:19 -0300

    Delete ironsource-ads-sdk/src/main/java/com/solodroidx/ads/util/Integration.java

--- ironsource-ads-sdk/src/main/java/com/solodroidx/ads/util/Integration.java
+++
@@ -1,7 +0,0 @@
-package com.solodroidx.ads.util;
-
-import com.ironsource.mediationsdk.integration.IntegrationHelper;
-
-public class Integration {
-  IntegrationHelper.validateIntegration(this);
-}
